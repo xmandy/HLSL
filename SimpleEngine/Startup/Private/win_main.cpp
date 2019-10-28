95075e3ad14af26aee47242e22446561f8305756
xmandy <shenxm06@gmail.com>
2019-10-28 19:47:52 +0800

    Finish Project Structure.

---
+++ SimpleEngine/Startup/Private/win_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "Platform/Public/Headers.h"
+#include "Platform/Public/Win32Window.h"
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World!" << "\n";
+	Platform::Win32Window Window;
+}
+
--- SimpleEngine/Startup/Src/win_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "Headers.h"
-
-int main(int argc, char** argv)
-{
-	std::cout << "Hello World!" << "\n";
-}
-
